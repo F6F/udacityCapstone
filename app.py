b362841973bf3622f1bb1533750665def4d01c3e
Tobias Hobmeier <tobias@antifuse.de>
2022-01-10 20:17:48 +0100

    add circle ci config requirements and app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Udacity Capstone project version: 0.0.1</p>"
+
+if __name__ == "__main__":
+    # load pretrained model as clf
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
